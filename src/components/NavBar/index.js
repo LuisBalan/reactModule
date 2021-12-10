ad8b42279943eeb16a198a9f25aa78e01507ab37
LuisBalan <jlbalcz@gmail.com>
2021-12-10 01:25:56 -0600

    add class03 exercise solution

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,58 +1,29 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from "react";
 
-
-import './NavBar.css'
-
-//think on what item is active
+// Styles
+import "./NavBar.css";
 
 function NavBar() {
-    const [active, setActive] = useState(0)
+	const [active, setActive] = useState(0);
 
-    const handleClick = (itemID) => {
-        setActive(itemID)
-    }
-
-    const checkActive = (itemID) => (active === itemID? "clickOn": "")
-    return(
-        <div>
-            <ul>
-                <button onClick = {() => setActive(1)} className = {active === 0? "clickOff":"clickOn"}>Item 1</button>
-                <button onClick = {() => setActive(1)} className = {active === 0? "clickOff":"clickOn"}>Item 2</button>
-                <button onClick = {() => setActive(1)} className = {active === 0? "clickOff":"clickOn"}>Item 3</button>
-            </ul>
-
-        </div>
-    )
-    
+	return (
+		<div className = 'itemsContainer'>
+			<ul>
+				<li className={active === 1? 'clickOn':''} onClick={() => setActive(1)}>
+					Item 1
+				</li>
+				<li className={active === 2? 'clickOn':''} onClick={() => setActive(2)}>
+					Item 2
+				</li>
+				<li className={active ===3? 'clickOn': ''} onClick={() => setActive(3)}>
+					Item 3
+				</li>
+			</ul>
+		</div>
+	);
 }
 
-
-
-// function NavBar(){
-//     return(
-//         <div>
-//             <ul>
-//                 <li className = "btn clickOn">Item 1</li>
-//                 <li className = "btn clickOff">Item 2</li>
-//                 <li className = "btn clickOff">Item 3</li>
-//             </ul>
-//         </div>
-//     )
-// }
-
-// function NavBar(){
-//     return(
-//         <div>
-//             {listItems}
-//         </div>
-
-//         )
-// }
-// export default NavBar
+export default NavBar;
 
 
 
-
-
-export default NavBar
