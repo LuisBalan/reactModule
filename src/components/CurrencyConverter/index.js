166dfc182c909a60cbc59e801ed1b9a6305c4204
LuisBalan <jlbalcz@gmail.com>
2022-01-05 23:14:43 -0600

    add select options with data bus

--- src/components/CurrencyConverter/index.js
+++ src/components/CurrencyConverter/index.js
@@ -3,6 +3,37 @@ import './CurrencyConverter.css'
 
 function Converter(){
 
+    // const currencies = ['Euro', 'GuatemalanQuetzal', 'MexicanPeso', 'PeruvianSol']  
+
+    const factors = [
+        {
+            name: 'Euro',
+            dolarEquivalence: 1.13
+
+        },
+        {
+            name: 'Guatemalan Quetzal',
+            dolarEquivalence: 0.13
+        },
+        {
+            name: 'Mexican Peso',
+            dolarEquivalence: 0.049
+        },
+        {
+            name: 'Peruvian Sol',
+            dolarEquivalence: 0.25
+        }
+    ];
+
+
+const option = factors.map((item) => {
+    return(
+        <option value = {item.name.split(' ').join('')}>{item.name}</option>
+    );
+});
+
+
+
     const [inputCurrency, setInputCurrency] = useState(0);
     const [outputCurrency, setOutputCurrency] = useState(0);
     const [inputExchange, setInputExchange] = useState('');
@@ -16,33 +47,27 @@ function Converter(){
         <div className="converter">
             <h2>Currency Exchange</h2>
             <div className="converterContainer">
-                {/* <div className = 'currencyToConvert'> */}
+
                     <form className = 'currencyToConvert'>
                         <label for = 'inputMoney'></label>
                         <input name = 'inputMoney' id = 'inputMoney'type = 'number'></input>
                         <label for = 'inputCurrency'></label>
                         <select multiple = {false} label = 'inputCurrency'>
-                            <option selected value = 'Mexican Peso'>Mexican Peso</option>
-                            <option value = 'Peruvian Sol'>Peruvian Sol</option>
-                            <option value = 'Guatemalan Quetzal'>Guatemalan Quetzal</option>
-                            <option value = 'Euro'>Euro</option>
+                            {option}
                         </select>
                     </form>
-                {/* </div> */}
-                {/* <div className="convertionResult"> */}
+
+
                 <form className="convertionResult">
                         <label for = 'ouputMoney'></label>
                         <input name = 'outputMoney' id = 'outputMoney' type = 'number'></input>
                         <label for = 'outputCurrency'></label>
                         <select name = 'outputCurrency' multiple = {false} id = 'outputCurrency'
                         onChange={event => setOutputCurrency({outputCurrency: event.target.value})}>
-                            <option value = 'Mexican Peso'>Mexican Peso</option>
-                            <option value = 'Peruvian Sol'>Peruvian Sol</option>
-                            <option value = 'Guatemalan Quetzal'>Guatemalan Quetzal</option>
-                            <option value = 'Euro'>Euro</option>
+                            {option}
                         </select>
                     </form>
-                {/* </div> */}
+
             </div>
     </div>
     )
